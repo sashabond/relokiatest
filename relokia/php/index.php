dfcac680898a2553d6647df0ce22b5e347afd75d
sashabond <78707006+sashabond@users.noreply.github.com>
2021-02-14 16:09:23 +0200

    Add files via upload

--- relokia/php/index.php
+++ relokia/php/index.php
@@ -1,36 +1,67 @@
 <?php 
 require 'vendor/autoload.php';
-$api_key = "API_KEY";
-$password = "1234";
-$yourdomain = "YOUR_DOMAIN";
 
-$url = "https://$yourdomain.freshdesk.com/api/v2/tickets?";
-$ch = curl_init($url);
+class UserInfo
+{
+    private $api_key = "API_KEY", $password = "1234";
+    public $ch, $url, $yourdomain = "YOUR_DOMAIN";
 
-curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_USERPWD, "$api_key:$password");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    public function ConnectionSettings(){
+        curl_setopt($this->ch, CURLOPT_HEADER, true);
+        curl_setopt($this->ch, CURLOPT_USERPWD, "$this->api_key:$this->password");
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+    }
 
-$server_output = curl_exec($ch);
-$info = curl_getinfo($ch);
-$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-$headers = substr($server_output, 0, $header_size);
-$response = substr($server_output, $header_size);
- 
-if($info['http_code'] == 200) {
-  echo "Tickets fetched successfully, the response is given below \n";
-  $fp = fopen('file.csv', 'w');
-	for($i=0;$i<9999;$i++){
-		$list = array (
-			array($headers[$i]),
-			array($response[$i])
-		);
-	fputcsv($fp, $list);
-	} 
-else {
-    echo "Error \n";
 }
-curl_close($ch);
-fclose($fp);
 
+class Output extends UserInfo{
+    public $server_output, $info, $header_size, $headers, $response, $fp, $list;
+
+    public function GetHeaders() {
+        $this->headers = substr($this->server_output, 0, $$this->header_size);
+        return $this->headers;
+    }
+    public function GetResponse() {
+        $this->response = substr($this->server_output, $this->header_size);
+        return $this->headers;
+    }
+
+    public function TicketOut(){
+        $this->fp = fopen('file.csv', 'w');
+        for ($i = 0; $i < 9999; $i++) {
+            $this->list = array(
+                array($this->headers[$i]),
+                array($this->response[$i])
+            );
+            fputcsv($this->fp, $this->list);
+        }
+        fclose($this->fp);
+    }
+}
+
+
+$out = new Output();
+
+$out->url = "https://$out->yourdomain.freshdesk.com/api/v2/tickets?";
+$out->ch = curl_init($out->url);
+$out->ConnectionSettings();
+
+$out->server_output = curl_exec($out->ch);
+$out->info = curl_getinfo($out->ch);
+$out->header_size = curl_getinfo($out->ch, CURLINFO_HEADER_SIZE);
+$out->GetHeaders();
+$out->GetResponse();
+
+
+if ($out->info['http_code'] == 200)
+    {
+    echo "Tickets fetched successfully, the response is given below \n";
+    $out->TicketOut();
+    }
+else
+    {
+        echo "Error \n";
+    }
+
+curl_close($out->ch);
 ?>
